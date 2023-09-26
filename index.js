f2bafc454c6d4d7a5cc246ab492af958fb10f5e1
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-09-26 10:04:26 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const cors = require("cors")
 const middlewares = jsonServer.defaults()
-const PORT = process.env.PORT || 8000
+const PORT =  8080
 
 server.use(cors())
 server.use(middlewares)
