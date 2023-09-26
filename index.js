e698aa60d020583f72ed8771d997ee482ce15f22
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-09-26 08:57:50 +0530

    add

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
+const cors = require("cors")
 const middlewares = jsonServer.defaults()
 const PORT = process.env.PORT || 8000
 
+server.use(cors())
 server.use(middlewares)
 server.use(router)
 server.listen(PORT, () => {
