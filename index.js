a6d3b3e990ef926a385932a7dd9cd45b8d0041ef
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-09-26 16:01:35 +0530

    add

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const cors = require("cors")
+require("dotenv").config()
 const middlewares = jsonServer.defaults()
 const PORT =  process.env.PORT || 5000
+// const corsObj = {
+//   origin : "http://localhost:3000",
+//   credentials:true
+// }
 
-const corsObj = {
-  origin : "http://localhost:3000",
-  credentials:true
-}
-
-server.use(cors(corsObj))
+server.use(cors())
 server.use(middlewares)
 server.use(router)
+
 server.listen(PORT, () => {
-  console.log('JSON Server is running')
+  console.log(`JSON Server is running on ${PORT}`)
 })
